0771b3481c3ac93d46392094e06a75a909410fd3
Jim <jim@phoenix-5.com>
2022-12-28 13:34:57 +0800

    add Reflection

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Reflection;
+
+Animal animal = new Animal("cat");
+
+var name = typeof(Animal).GetMethod("GetName", BindingFlags.NonPublic | BindingFlags.Instance)
+    .Invoke(animal, Array.Empty<object?>());
+
+
+Console.WriteLine(name);
+
+
+
+public class Animal
+{
+    private string _name { get; set; }
+
+    internal Animal(string name)
+    {
+        _name = name;
+    }
+
+    private string GetName()
+    {
+        return _name;
+    }
+
+    private void SetName(string name)
+    {
+        _name = name;
+    }
+    
+}
